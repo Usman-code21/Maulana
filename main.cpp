e957c06b75f42385c0e9ed4f3ea91f670d91efe1
Maulaa <maulausman185@gmail.com>
2025-02-10 03:26:35 -1200

    Perubahan Pertama

--- main.cpp
+++ main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
+using namespace std;
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "PEMBUATAN NAMETAG SANTRI" << std::endl;
+    cout << "=================================\n";
+    cout << "Masukan Nama Santri : ";
+    std::string nama;
+    std::cin >> nama;"\n";
+    std::cout << "Masukan Kelas \t : ";
+    int kelas;
+    std::cin >> kelas;"\n";
+    std::string pita;
+
+    cout << "Masukan Nama Konsulat :";
+    std::string konsulat;
+    std::cin >> konsulat;"\n";
+
+    cout << "NAMETAGE SANTRI  \n";
+    std::cout << "Nama Santri \t : "<< nama << "\n";
+    cout << "Kelas \t\t : " << kelas << "\n";
+    cout << "Warna Pita \t : " ;
+    switch (kelas) {
+        case 7:
+            cout << "Pita Warna Hijau \n";
+        break;
+        case 8:
+            cout << "Pita Warna Biru \n";
+        break;
+        case 9:
+            cout << "Pita Warna Hitam \n";
+        break;
+        default:
+            cout << "Kelas Santri Tdak Ditemukan \n";
+    }
+    cout << "Konsulat \t : " << konsulat << "\n";
+
     return 0;
 }
